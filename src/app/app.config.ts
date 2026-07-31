ab67f6ee37d469093ee84ba13230d6f2574829fd
saiyan3.14 <141233559+SaiyanPi@users.noreply.github.com>
2026-07-31 19:18:30 +0545

    updated app.config.ts for automatic binding CSS class to login from fields class. Note this works only in signal forms.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { routes } from './app.routes';
 import { provideRouter } from '@angular/router';
+import { provideSignalFormsConfig } from '@angular/forms/signals';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideSignalFormsConfig({
+      classes: {
+        'is-invalid': field => field.state().invalid() && field.state().touched()
+      }
+    })
   ]
 };
