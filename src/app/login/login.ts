c3c3c7c6424af12832c8a4fe6735a0f351a473c3
saiyan3.14 <141233559+SaiyanPi@users.noreply.github.com>
2026-08-02 22:01:59 +0545

    updated login component for material's snackBar notification popups. Updated login template for better UI

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -6,16 +6,18 @@ import { AuthService } from '../services/auth-service';
 import { LoginRequestModel } from '../models/auth/login-request-model';
 import { HttpErrorResponse } from '@angular/common/http';
 import { ApiErrorResponseModel } from '../models/error/api-error-response-model';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'ep-login',
-  imports: [FormField, FormRoot],
+  imports: [FormField, FormRoot, MatSnackBarModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
   private readonly authService = inject(AuthService);
   private readonly router = inject(Router);
+  private readonly snackBar = inject(MatSnackBar);
 
   // protected readonly loginFailed = signal(false);
   protected readonly serverValidationErrors = signal<Record<string, string[]>>({});
@@ -42,7 +44,7 @@ export class Login {
   private async login() {
     // this.loginFailed.set(false);
     // Clearing serverValidationErrors them before every login attempt:
-    this.serverValidationErrors.set({}); 
+    this.serverValidationErrors.set({});
 
     const { email, password } = this.credentials().value();
     const request: LoginRequestModel = { email, password };
@@ -53,24 +55,26 @@ export class Login {
       return;
     } catch(error) {
       if (error instanceof HttpErrorResponse) {
-
         const apiError = error.error as ApiErrorResponseModel;
 
-        this.serverValidationErrors.set(apiError.details ?? {});
-
-        // const errors = [{ kind: 'server', message: apiError.message }];
-        // if (apiError.details) {
-        //   for (const messages of Object.values(apiError.details)) {
-        //     for (const message of messages) {
-        //       errors.push({ kind: 'validation', message });
-        //     }
-        //   }
-        // }
-        // return errors;
-        
-        return [{ kind: 'server', message: apiError.message }];
+        if (apiError.details) {
+          // Display field-level validation errors.
+          this.serverValidationErrors.set(apiError.details);
+          // 👇use snackBar only for general error not for field specific error
+          // this.snackBar.open(
+          //   apiError.message,
+          //   'Close', { duration: 5000}
+          // );
+        } else {
+          // Display a general error.
+          this.snackBar.open(apiError.message, 'Close', {
+            duration: 5000,
+            panelClass: ['error-snackbar']
+          });
+        }
 
       }
+
       return [{ kind: 'server', message: 'Something went wrong.' }];
     }
   }
